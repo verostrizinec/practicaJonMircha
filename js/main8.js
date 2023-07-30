1c49fa9e78c6bd66a9463d694391364f14c4a3f0
Vero Strizinec <verostrizinec@gmail.com>
2023-07-30 16:59:50 -0300

    agregando clase 50 y 51

--- js/main8.js
+++ js/main8.js
@@ -44,4 +44,90 @@ console.log(Object.getOwnPropertySymbols(persona)); // Esto me da la lista de sy
 
 console.clear();
 
-// ***** CLASE 50:  *****
+// ***** CLASE 50: SETS *****
+
+// SETS es un nuevo tipo de dato desde 2015. Set solo acepta valores unicos, de valores primitivos.
+
+const SET = new Set([1,2,3,3,4,5,true,false,false,{}, {}, "hola", "HOLA"]);
+console.log(SET);
+console.log(SET.size); // size es para saber el tamaño de un set
+
+// otro set
+const SET2 = new Set();
+SET2.add(1);
+SET2.add(2);
+SET2.add(2);
+SET2.add(3);
+SET2.add(true);
+SET2.add(false);
+SET2.add(true);
+SET2.add({});
+
+console.log(SET2);
+/*
+console.log("Recorriendo set");
+
+for (item of SET) {
+    console.log(item);
+}
+
+console.log("Recorriendo set");
+SET2.forEach(item => console.log((item))); // imprime cada item y mandalo a la consola
+*/
+
+let arr = Array.from(SET); // convierte en un iterable de tipo arreglo
+console.log(arr);
+console.log(arr[3]); // imprime la posicion 3
+
+SET.delete("HOLA"); // para eliminar lo que le pido
+console.log(SET);
+
+console.log(SET.has("hola")); // has es para saber si existe el dato o no
+
+SET2.clear(); // esto es para borrar un set
+console.log(SET2);
+
+console.clear();
+
+// ***** CLASE 51: MAP *****
+
+// Es un tipo de dato que es un objeto, sirve para almacenar un conjunto de valores asociados. Se puden agregar elementos con "set"
+
+const mapa = new Map(); // trabaja similara los getters y setters de objetos
+mapa.set("nombre", "Vero");
+mapa.set("apellido", "Strizinec");
+mapa.set("edad", 34);
+
+// en los mapas podemos generar llaves que no son cadenas de texto
+
+mapa.set(19, "diecinueve");
+mapa.set(false, "falso");
+
+console.log(mapa);
+console.log(mapa.has("Nombre"));
+mapa.set("nombre", "Vero Strizinec"); // asi cambiamos la propiedad
+console.log(mapa.get("nombre")); // asi cambiamos la propiedad
+
+// con un for of recorremos un mapa
+
+for (let [key, value] of mapa) {
+    console.log(`Llave: ${key}, Valor: ${value}`);
+}
+
+// nuevo mapa
+
+const mapa2 = new Map([
+    ["nombre", "Morena"],
+    ["edad", 10],
+    ["animal", "perro"],
+]);
+
+console.log(mapa2);
+
+// almacenar como arreglo toda la lista de llaves y valores con destructuracion
+
+const llavesMapa2 = [...mapa2.keys()]; // del mapa 2 almacena las llaves
+const valoresMapa2 = [...mapa2.values()]; // del mapa 2 almacena los valores
+
+console.log(llavesMapa2);
+console.log(valoresMapa2);
